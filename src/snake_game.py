9a541d42bbce76bd894734b1c3d3c1623205d20e
joaoperfig <joaoperfig@gmail.com>
2019-01-15 03:55:00 +0100

    add tail flag functionality; fix update rendering; fix collision check

--- src/snake_game.py
+++ src/snake_game.py
@@ -21,10 +21,11 @@ class SnakeGame:
         for i in range(height):
             self.matrix += [[0]*width] # 0=empty 1=snake 2=fruit
         self.snake = []
-        self.snake_directrion = Direction.left
-        self.tail_flag = False #Hold tail for one step
+        self.snake_direction = Direction.up
+        self.tail_flag = True #Hold tail for one step
         self.score = 0
         self.game_over = False;
+        self.shown = False
         
     def set(self, position, value): #Change value at position, position is tuple (x, y)
         self.matrix[position[1]][position[0]] = value
@@ -41,13 +42,17 @@ class SnakeGame:
         self.set(position, 1)
 
     def display(self):
-        plt.matshow(self.matrix)
-        plt.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False, labeltop=False)
-        plt.summer()
-        plt.title('SnakeGame')
-        plt.text(-1,-1,'Score = ' + str(self.score))
-        plt.show()
-        return
+        if not self.shown:
+            self.shown = True
+            plt.matshow(self.matrix)
+            plt.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False, labeltop=False)
+            plt.summer()
+            plt.title('SnakeGame')
+            plt.text(-1,-1,'Score = ' + str(self.score))
+            return
+        else:
+            plt.matshow(self.matrix, 0)
+            return
     
     def run(self, steptime): #call step ever x seconds
         while not self.game_over:
@@ -55,39 +60,45 @@ class SnakeGame:
             self.step()
             if (self.score%1000 == 100): self.add_fruit();
             time.sleep(steptime)
+            plt.pause(0.0001)
         return
         
     def step(self): #move snake forward, check tail flag, add tiny score
-        head_position = (self.snake[0][0] + self.snake_directrion[0], self.snake[0][1] + self.snake_directrion[1])
-        if (self.check_collision(head_position)):
+        head_position = (self.snake[0][0] + self.snake_direction[0], self.snake[0][1] + self.snake_direction[1])
+        if (self.check_collision()):
             self.game_over = True
         else:
+            print(self.snake)
             self.set(head_position, 1)
-            self.remove(self.snake[len(self.snake) - 1])
-            for i in range(len(self.snake)-1,0,-1):
-                position = (self.snake[i-1][0], self.snake[i-1][1])
-                self.snake[i] = position
-                self.set(position, 1)
-            self.snake[0] = head_position
+            self.snake = [head_position] + self.snake
+            if self.tail_flag:
+                self.tail_flag = False
+            else:
+                self.remove(self.snake[len(self.snake) - 1])
+                self.snake = self.snake[:-1]
         self.score += 1
         return
 
     def move_snake(self,  direction): #change snake direction (ask agent?)
         return
     
-    def check_collision(self, position): #out of bounds or ran over itself (repeated position in snake positions)
-        if self.get(position) == 1:
+    def check_collision(self): #out of bounds or ran over itself (repeated position in snake positions)
+        if self.snake[0] in self.snake[1:]: #Head hits body
             return True
-        else:
-            return False
+        if self.snake[0][0] < 0:
+            return True
+        if self.snake[0][1] < 0:
+            return True        
+        if self.snake[0][0] >= self.width:
+            return True
+        if self.snake[0][0] >= self.height:
+            return True          
+        
     
     def check_ate_fruit(self): #check head on fruit, use tail_flag, add score
         if(self.fruit in self.snake):
             tail_flag = True
             self.score += 100
-        else:
-            tail_flag = False
-        return
     
     def add_fruit(self): #set something to 2 (do not put over snake)
         pos = self.snake[0]
